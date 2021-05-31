87a1e9c502178b71f528cc799ea65da7e7e4e4ae
Stoyan Valkov <m0673n@abv.bg>
2021-05-31 03:04:46 +0300

    Uploading homework.

---
+++ 06_file_handling/excercise/04_problem_solution.py
@@ -0,0 +1,27 @@
+import os
+
+
+def get_info():
+    files = {}
+    for filename in os.listdir():
+        if os.path.isfile(filename):
+            name, extension = filename.split(".")
+            if extension not in files:
+                files[extension] = [name]
+            else:
+                files[extension].append(name)
+    files = dict(sorted(files.items(), key=lambda x: (x[0], x[1])))
+    return files
+
+
+def deliver_result(info):
+    username = os.getlogin()
+    with open(f'C:\\Users\\{username}\\Desktop\\report.txt', 'w') as file:
+        for extension in info:
+            file.write("." + extension + "\n")
+            for name in info[extension]:
+                file.write(f"- - - {name}.{extension}\n")
+
+
+data = get_info()
+deliver_result(data)
