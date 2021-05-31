586b632a8179e5b93803d99542e66df849a47685
Stoyan Valkov <m0673n@abv.bg>
2021-05-31 21:11:39 +0300

    Minor improvements.

--- 06_file_handling/excercise/03_problem_solution.py
+++ 06_file_handling/excercise/03_problem_solution.py
@@ -1,37 +1,42 @@
 import os
 
+
+def create_file(file_name):
+    file = open(file_name, "w")
+    file.close()
+
+
+def add_info(file_name, content):
+    with open(file_name, "a") as file:
+        file.write(content + "\n")
+
+
+def replace_info(file_name, old_string, new_string):
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            data = file.read()
+            data = data.replace(old_string, new_string)
+        with open(file_name, "w") as file:
+            file.write(data)
+    else:
+        print("An error occurred")
+
+
+def delete_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    else:
+        print("An error occurred")
+
+
+actions = {"Create": create_file, "Add": add_info, "Replace": replace_info, "Delete": delete_file}
+
 while True:
     command = input()
 
     if command == "End":
         break
 
-    command = command.split("-")
-    file_name = command[1]
-
-    if command[0] == "Create":
-        file = open(file_name, "w")
-        file.close()
-
-    elif command[0] == "Add":
-        content = command[2]
-        with open(file_name, "a") as file:
-            file.write(content + "\n")
-
-    elif command[0] == "Replace":
-        old_string = command[2]
-        new_string = command[3]
-        if os.path.exists(file_name):
-            with open(file_name, "r") as file:
-                data = file.read()
-                data = data.replace(old_string, new_string)
-            with open(file_name, "w") as file:
-                file.write(data)
-        else:
-            print("An error occurred")
-
-    elif command[0] == "Delete":
-        if os.path.exists(file_name):
-            os.remove(file_name)
-        else:
-            print("An error occurred")
+    args = command.split("-")
+    action = args.pop(0)
+    actions[action](*args)
