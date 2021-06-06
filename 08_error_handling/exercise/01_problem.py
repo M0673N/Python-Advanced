85edb13a149ab13ef8c4293ed95c49cf2f8e23f6
Stoyan Valkov <m0673n@abv.bg>
2021-06-07 00:54:12 +0300

    Added some changes due to misread problem description.

--- 08_error_handling/exercise/01_problem.py
+++ 08_error_handling/exercise/01_problem.py
@@ -1,37 +1,33 @@
 numbers_dictionary = {}
 
 line = input()
-
-while line != "Search":
-    number_as_string = line
-    try:
+try:
+    while line != "Search":
+        number_as_string = line
         number = int(input())
         numbers_dictionary[number_as_string] = number
-    except ValueError:
-        print("The variable number must be an integer")
+
+        line = input()
 
     line = input()
 
-line = input()
-
-while line != "Remove":
-    searched = line
-    try:
+    while line != "Remove":
+        searched = line
         print(numbers_dictionary[searched])
-    except KeyError:
-        print("Number does not exist in dictionary")
+
+        line = input()
 
     line = input()
 
-line = input()
-
-while line != "End":
-    searched = line
-    try:
+    while line != "End":
+        searched = line
         del numbers_dictionary[searched]
-    except KeyError:
-        print("Number does not exist in dictionary")
 
-    line = input()
+        line = input()
+
+except ValueError:
+    print("The variable number must be an integer")
+except KeyError:
+    print("Number does not exist in dictionary")
 
 print(numbers_dictionary)
